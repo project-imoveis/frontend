08089228aa852daf2572d039bda3a220b462bdbc
EmersonTeles <emersonteles21@gmail.com>
2023-03-03 22:23:49 -0300

    removing carousel and update names

--- src/components/propertyCard/index.tsx
+++ src/components/propertyCard/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
-import property from "../../assets/property.jpg";
-import Carousel from "../carousel";
+import propertyPhoto1 from "../../assets/property.jpg";
+import propertyPhoto2 from "../../assets/property2.jpg";
+import propertyPhoto3 from "../../assets/building.jpg";
 import { PropertyType } from "@/@core/models/property_model";
 
 type PropertyCardProps = {
@@ -8,18 +9,21 @@ type PropertyCardProps = {
   property: PropertyType;
 };
 export default function PropertyCard({ key, property }: PropertyCardProps) {
+  function randomImage() {
+    const images = [propertyPhoto1, propertyPhoto2, propertyPhoto3];
+    const random = Math.floor(Math.random() * images.length);
+    return images[random];
+  }
+
   return (
     <article className="propertyCard">
       <div className="propertyCard_image">
-        {
-          //<Image src={property} alt="" />
-        }
-        <Carousel infiniteLoop={false} />
+        <Image src={randomImage()} alt="foto do imóvel" />
       </div>
       <div className="propertyCard_description">
         <h2>{property.post_type}</h2>
-        <h1>{property.titulo}</h1>
-        <p>{property.area_util}m² - 2 quartos - 1 banheiro - 1 vaga</p>
+        <h1>{property.title}</h1>
+        <p>{property.useful_area}m² - 2 quartos - 1 banheiro - 1 vaga</p>
       </div>
     </article>
   );
