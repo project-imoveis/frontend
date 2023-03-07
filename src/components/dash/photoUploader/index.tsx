29a7262c4e75459e1b113ea16456635a196ef709
EmersonTeles <emersonteles21@gmail.com>
2023-03-07 02:22:35 -0300

    feat: turn Dropzone & PreviewGroup into components

--- src/components/dash/photoUploader/index.tsx
+++ src/components/dash/photoUploader/index.tsx
@@ -1,24 +1,20 @@
 import { useEffect, useState } from "react";
-import Image from "next/image";
-import { BsTrash } from "react-icons/bs";
-import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
 import { useDropzone } from "react-dropzone";
 import api from "@/@core/providers/api";
-type Photo = {
-  blob: string;
-  name: string;
-  size: number;
-  extension: string;
-};
+import PreviewGroup from "./PreviewGroup";
+import { Dropzone } from "./Dropzone";
+
 export function PhotoUploader() {
   const [photos, setPhotos] = useState<any[]>([]);
   const [isFull, setIsFull] = useState(false);
-  const { getRootProps, getInputProps } = useDropzone({
+  const { getRootProps, getInputProps, isDragActive, isDragReject } = useDropzone({
     accept: {
-      "image/*": [],
+      "image/jpeg": [],
+      "image/png": [],
     },
     onDrop: (acceptedFiles) => {
       if (photos.length + acceptedFiles.length > 20) return setIsFull(true);
+      console.log(photos);
       setPhotos((oldPhotos) => [
         ...oldPhotos,
         ...acceptedFiles.map((file) =>
@@ -30,29 +26,6 @@ export function PhotoUploader() {
       if (photos.length == 20) setIsFull(true);
     },
   });
-
-  function readURL(event: any) {
-    const extension = event.target.files[0].name.split(".").pop();
-    const extensionsAllowed = ["jpg", "png", "jpeg"];
-    if (!event.target.files[0]) return;
-    for (let i = 0; i < event.target.files.length; i++) {
-      if (!extensionsAllowed.includes(extension)) continue;
-      setupReader(event.target.files[i]);
-    }
-  }
-  async function setupReader(file: any) {
-    const reader = new FileReader();
-    reader.onload = (event: any) => {
-      const photo: Photo = {
-        name: file.name,
-        size: file.size,
-        extension: file.type,
-        blob: event.target.result,
-      };
-      setPhotos((oldPhotos) => [...oldPhotos, photo]);
-    };
-    await reader.readAsDataURL(file);
-  }
   function removePhoto(id: number) {
     setPhotos((oldPhotos) => oldPhotos.filter((photo, index) => index !== id));
     setIsFull(false);
@@ -83,85 +56,16 @@ export function PhotoUploader() {
   return (
     <div className="photoUploader">
       <h2>Escolha suas melhores fotos</h2>
-      <DragDropContext onDragEnd={onDragEnd}>
-        <Droppable droppableId="droppable" type="COLUMN" direction="horizontal">
-          {(provided) => (
-            <section
-              className="photoUploader_group"
-              {...provided.droppableProps}
-              ref={provided.innerRef}
-            >
-              <div
-                {...getRootProps({
-                  className: `dropzone 
-                  ${isFull ? "disabled" : ""}`,
-                })}
-              >
-                <label htmlFor="propertyPhotosInput" className="photoUploader_label">
-                  {isFull
-                    ? "Não é possível adicionar mais fotos, o limite foi atingido."
-                    : "Clique aqui para adicionar suas imagens"}
-                  <span className="photoUploader_span">
-                    {isFull
-                      ? "Remova alguma foto para continuar"
-                      : "*Formatos aceitos: jpg, png, jpeg"}
-                  </span>
-                </label>
-                <input
-                  className="photoUploader_input"
-                  id="propertyPhotosInput"
-                  type="file"
-                  multiple
-                  accept="image/jpg, image/png, image/jpeg"
-                  {...(isFull ? { disabled: true } : {})}
-                  {...getInputProps()}
-                />
-              </div>
-              <p>{photos.length}/20 fotos</p>
-              <div className="photoUploader_previewGroup">
-                {photos.map((photo, id) => (
-                  <Draggable key={photo.name} draggableId={photo.name} index={id}>
-                    {(provided) => (
-                      <div
-                        className="photoUploader_imageWrapper"
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        ref={provided.innerRef}
-                      >
-                        <h1 className="photoUploader_imageWrapper_title">Foto principal</h1>
-                        <button
-                          type="button"
-                          className="photoUploader_imageWrapper_close"
-                          onClick={() => removePhoto(id)}
-                        >
-                          <BsTrash />
-                        </button>
-                        <Image
-                          className="photoUploader_imageWrapper_image"
-                          placeholder="blur"
-                          blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQC"
-                          src={photo.preview}
-                          alt="foto"
-                          fill
-                          onLoad={() => {
-                            URL.revokeObjectURL(photo.preview);
-                          }}
-                        />
-                        <input
-                          className="photoUploader_imageWrapper_legenda"
-                          placeholder="Legenda..."
-                        />
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-              </div>
-            </section>
-          )}
-        </Droppable>
-      </DragDropContext>
-      <button type="button" className="photoUploader_button" onClick={Submit}>
+      <section className="photoUploader_group">
+        <Dropzone {...{ isFull, isDragReject, isDragActive, getRootProps, getInputProps }} />
+        <PreviewGroup photos={photos} removePhoto={removePhoto} onDragEnd={onDragEnd} />
+      </section>
+      <button
+        type="button"
+        className="photoUploader_button"
+        {...(photos.length == 0 ? { disabled: true } : {})}
+        onClick={Submit}
+      >
         Enviar
       </button>
     </div>
