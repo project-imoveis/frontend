f2996d2b9623d768fd810cdf6b687a6d02b2479d
EmersonTeles <emersonteles21@gmail.com>
2023-03-08 16:29:36 -0300

    changing propertyCard layout

--- src/components/propertyCard/index.tsx
+++ src/components/propertyCard/index.tsx
@@ -2,13 +2,19 @@ import Image from "next/image";
 import propertyPhoto1 from "../../assets/property.jpg";
 import propertyPhoto2 from "../../assets/property2.jpg";
 import propertyPhoto3 from "../../assets/building.jpg";
+import thaisLogo from "../../assets/thais.png";
 import { PropertyType } from "@/@core/models/property_model";
-
+import Meter from "../../assets/meter.svg";
+import Bedroom from "../../assets/bedroom.svg";
+import Bathroom from "../../assets/bathroom.svg";
+import { FaRegHeart, FaHeart } from "react-icons/fa";
+import { useState } from "react";
 type PropertyCardProps = {
   key: number;
   property: PropertyType;
 };
 export default function PropertyCard({ property }: PropertyCardProps) {
+  const [fav, setFav] = useState(false);
   function getMainImage() {
     if (property.Images.length > 0) {
       const mainImage = property.Images.find((image: any) => image.isMain == true);
@@ -19,16 +25,48 @@ export default function PropertyCard({ property }: PropertyCardProps) {
     const random = Math.floor(Math.random() * images.length);
     return images[random];
   }
-
+  function favProperty() {
+    setFav((old) => !old);
+  }
+  const getFavIcon = () => {
+    return fav ? <FaHeart /> : <FaRegHeart />;
+  };
   return (
     <article className="propertyCard">
       <div className="propertyCard_image">
         <Image src={getMainImage()} alt="foto do imóvel" fill />
       </div>
       <div className="propertyCard_description">
-        <h2>{property.post_type}</h2>
-        <h1>{property.title}</h1>
-        <p>{property.useful_area}m² - 2 quartos - 1 banheiro - 1 vaga</p>
+        <div className="propertyCard_description-group1">
+          <h1>{`${property.Address.neighborhood.split(" (")[0]}, ${property.Address.city}`}</h1>
+          <h2>{property.Address.street}</h2>
+          <button type="button" onClick={favProperty}>
+            {getFavIcon()}
+          </button>
+        </div>
+        <div className="propertyCard_description-group2">
+          <span>
+            <Meter />
+            {property.useful_area}m²
+          </span>
+          <span>
+            <Bedroom />2
+          </span>
+          <span>
+            <Bathroom />1
+          </span>
+        </div>
+        <div className="propertyCard_description-group3">
+          <h1>
+            {property.value.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+              maximumFractionDigits: 0,
+            })}
+          </h1>
+          <h2>+ R$ 230 Condomínio</h2>
+          <Image src={thaisLogo} alt="logo" />
+        </div>
       </div>
     </article>
   );
