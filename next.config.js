1fc45d1c167599b7e0213563225837623ed55949
EmersonTeles <emersonteles21@gmail.com>
2023-03-08 00:38:01 -0300

    adding s3 image remotePatterns

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "imoveis-upload.s3.sa-east-1.amazonaws.com",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
